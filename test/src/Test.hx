22bda883e3abc3aa232c470769d6f0df4a65bc3e
HuiWan <hw05548@mun.ca>
2017-04-07 20:17:02 -0400

    fix bugs and add initial buttons and click functions for the gate generate

--- test/src/Test.hx
+++ test/src/Test.hx
@@ -13,9 +13,7 @@ class Test {
 
     static public function main() {
         canvas = cast Browser.document.getElementById("canvas");
-        //draw and gate
         cxt = untyped canvas.getContext("2d");
-        var scale = Browser.window.devicePixelRatio;
 
         var backingStoreRatioDynamic : Dynamic = Reflect.field( cxt, "webKitBackingStorePixelRatio" ) ;
         if( backingStoreRatioDynamic == null )
@@ -30,21 +28,12 @@ class Test {
         if( backingStoreRatioDynamic == null ) 1.0
         else cast( backingStoreRatioDynamic, Float ) ;
 
-        var pixelRatio = (Browser.window.devicePixelRatio || 1)/backingStoreRatioDynamic;
+        var pixelRatio = Browser.window.devicePixelRatio/backingStoreRatio;
         trace(pixelRatio);
 
-
         var drawingAdapter:DrawingAdapterI = new DrawingAdapter(cxt);
+
         var circuitDiagram:CircuitDiagram = new CircuitDiagram();
-
-//        var component_:ComponentKind = Type.createInstance(Type.resolveClass("com.mun.model.gates.OR"),[]);
-////        var and_gate:ComponentKind = new AND();
-//        var component__:Component = new Component(250, 50, 40, 40, Orientation.EAST, component_, 2);
-////        var drawAnd:DrawComponent = new DrawAND(component__, drawingAdapter);
-//        var draw:DrawComponent = Type.createInstance(Type.resolveClass("com.mun.view.drawComponents.DrawOR"),[component__,drawingAdapter]);
-//        draw.drawCorrespondingComponent();
-
-//        new CreateAndDraw("OR",250, 50, 40, 40, Orientation.EAST, 3, drawingAdapter);
         new ButtonClick(drawingAdapter);
     }
 }
