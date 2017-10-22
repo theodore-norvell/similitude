6b5cb8ad050caf31d1369a396011616db3c72459
Theodore Norvell <theo@mun.ca>
2017-10-22 11:59:18 -0230

    Added Assert macro.

--- client/src/Test.hx
+++ client/src/Test.hx
@@ -1,15 +1,10 @@
 package ;
 
 import com.mun.controller.controllerState.FolderState;
-import com.mun.assertions.Assert;
 
-// TODO Rename this class to Main
+// TODO Rename this class to ClientMain
 class Test {
     static public function main() {
-        trace( "Hello") ;
-        var a = 2 ;
-        var b = 3 ;
-        Assert.assert( a==b ) ;
         var folderState:FolderState = new FolderState() ;
     }
 }
--- client/src/com/mun/assertions/Assert.hx
+++ client/src/com/mun/assertions/Assert.hx
@@ -1,17 +1,63 @@
 package com.mun.assertions;
 
+import haxe.macro.Expr;
+using haxe.macro.Tools;
 
+/** Assertions. Based on code by Simon Krajewski.
+* See https://code.haxe.org/category/macros/assert-with-values.html
+*
+**/
 class Assert {
-    static public function assert( e:Bool ) {
-        if( !e ) throw new AssertionFailure() ;
+    static public macro function assert(e:Expr) {
+        var s = e.toString();
+        var p = e.pos;
+        var el = [];
+        var descs = [];
+        function add(e:Expr, s:String) {
+            var v = "_tmp" + el.length;
+            el.push(macro var $v = $e);
+            descs.push(s);
+            return v;
+        }
+        function map(e:Expr) {
+            return switch (e.expr) {
+                case EConst((CInt(_) | CFloat(_) | CString(_) | CRegexp(_) | CIdent("true" | "false" | "null"))):
+                    e;
+                case _:
+                    var s = e.toString();
+                    e = e.map(map);
+                    macro $i{add(e, s)};
+            }
+        }
+        var e = map(e);
+        var a = [for (i in 0...el.length) macro { expr: $v{descs[i]}, value: $i{"_tmp" + i} }];
+        el.push(macro if (!$e) {
+            var e = new AssertionFailure($v{s}, $a{a}) ;
+            @:pos(p) trace("Throwing exception "+e) ;
+            @:pos(p) throw e;});
+        return macro $b{el};
     }
 }
 
+private typedef AssertionPart = {
+    expr: String,
+    value: Dynamic
+}
 
 class AssertionFailure {
-    public function new() { }
+    public var message(default, null):String;
+    public var parts(default, null):Array<AssertionPart>;
+    public function new(message:String, parts:Array<AssertionPart>) {
+        this.message = message;
+        this.parts = parts;
+    }
 
     public function toString() {
-        return "Assertion failure" ;
+        var buf = new StringBuf();
+        buf.add("Assertion failure: " + message + ", where");
+        for (part in parts) {
+            buf.add("\n\t" + part.expr + ": " + part.value);
+        }
+        return buf.toString();
     }
 }
