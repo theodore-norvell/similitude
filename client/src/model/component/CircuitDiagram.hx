c4e51c21c1cb0a562ac3bba8e970dea003266ad3
Theodore Norvell <theo@mun.ca>
2020-03-29 18:26:31 -0230

    Clear the canvas prior to redawing.

--- client/src/model/component/CircuitDiagram.hx
+++ client/src/model/component/CircuitDiagram.hx
@@ -154,6 +154,7 @@ class CircuitDiagram implements CircuitDiagramI implements Observer{
         }
         
         updateBoundingBox() ;
+
     }
 
     /**
--- client/src/view/View.hx
+++ client/src/view/View.hx
@@ -105,7 +105,10 @@ class View
 	
 	public function updateCanvas() : Void {
 		Console.log('view.updateCanvas');
-		var drawingAdapter = new DrawingAdapter(Transform.identity(), this.activeTab.canvasElement.getContext2d());
+		var canvas = this.activeTab.canvasElement ;
+		var context = canvas.getContext2d() ;
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		var drawingAdapter = new DrawingAdapter(Transform.identity(), context);
 		this.activeTab.tabModel.draw(drawingAdapter);
 	}
 	
